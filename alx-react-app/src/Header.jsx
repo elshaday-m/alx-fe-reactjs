9f7546b420f37a1e0d097e282923a1c0254b0bbe
yohannes <yohanneshailemariam75@gmail.com>
2025-11-02 08:25:18 -0500

    resolve errors

--- alx-react-app/src/Header.jsx
+++ alx-react-app/src/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Header = () => {
+function Header() {
   return (
     <header>
       <h1>My Favorite Cities</h1>
     </header>
   );
-};
+}
 
 export default Header;
--- alx-react-app/src/MainContent.jsx
+++ alx-react-app/src/MainContent.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const MainContent = () => {
+function MainContent() {
   return (
     <main>
       <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
-};
+}
 
 export default MainContent;
