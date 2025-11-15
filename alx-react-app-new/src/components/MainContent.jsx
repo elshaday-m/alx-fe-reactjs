d950cdfcd2d5074b122a69be8a97ac081433bac6
yohannes <yohanneshailemariam75@gmail.com>
2025-11-15 05:56:32 -0500

    feat: react-new-app

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <main
+      style={{
+        backgroundColor: "#e5e5ff",
+        padding: "20px",
+        marginTop: "10px",
+        textAlign: "center",
+        fontSize: "18px",
+      }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
---
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const UserProfile = (props) => {
+  return (
+    <div
+      style={{
+        border: "1px solid gray",
+        padding: "15px",
+        margin: "15px auto",
+        width: "300px",
+        borderRadius: "8px",
+        backgroundColor: "#f9f9f9",
+      }}
+    >
+      <h2 style={{ color: "blue", marginBottom: "10px" }}>{props.name}</h2>
+
+      <p style={{ fontSize: "16px" }}>
+        Age: <span style={{ fontWeight: "bold" }}>{props.age}</span>
+      </p>
+
+      <p style={{ fontSize: "15px", color: "#333" }}>Bio: {props.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
