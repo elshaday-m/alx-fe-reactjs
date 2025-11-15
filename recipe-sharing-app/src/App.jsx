137500ef24be12c8f6cc6625c5bc86330b5f7d0c
yohannes <yohanneshailemariam75@gmail.com>
2025-11-15 08:45:35 -0500

    fix: rename FavoritesList

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,7 +4,7 @@ import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 import SearchBar from "./components/SearchBar";
-import FavoritesList from "./components/FavouritesList";
+import FavoritesList from "./components/FavoritesList";
 import RecommendationsList from "./components/RecommendationsList";
 import { useRecipeStore } from "./components/recipeStore";
 
