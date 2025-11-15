113d8bffbb6dcebb2dace09fde2e8783c5211395
yohannes <yohanneshailemariam75@gmail.com>
2025-11-15 08:27:01 -0500

    fix: modify file paht of some components

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,9 +4,9 @@ import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 import SearchBar from "./components/SearchBar";
-import FavoritesList from "./components/FavoritesList";
+import FavoritesList from "./components/FavouritesList";
 import RecommendationsList from "./components/RecommendationsList";
-import { useRecipeStore } from "./recipeStore";
+import { useRecipeStore } from "./components/recipeStore";
 
 function HomePage() {
   const generateRecommendations = useRecipeStore(
