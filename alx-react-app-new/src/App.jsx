4c8a4bf29f0b49ffba200116e799b61b249f679d
yohannes <yohanneshailemariam75@gmail.com>
2025-11-15 06:38:26 -0500

    chore: import counter.jsx in app.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,6 +20,8 @@ function App() {
       <WelcomeMessage />
 
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+
+      <Counter />
     </>
   );
 }
