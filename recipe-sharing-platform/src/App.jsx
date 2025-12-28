560f0935f3c6b761a2ba11711ccf30b1c7a2733c
yohannes <yohanneshailemariam75@gmail.com>
2025-12-28 16:44:15 +0300

    feat: recipe sharing platform

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind + Vite is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"], // scan these files for Tailwind classes
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
