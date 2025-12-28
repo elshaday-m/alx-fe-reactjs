8aa21c53317209099d4ebdafaa0fb00a02354ab9
yohannes <yohanneshailemariam75@gmail.com>
2025-12-28 15:57:34 +0300

    feat: react query demo

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,10 +1,18 @@
+import React from "react";
 import PostsComponent from "./PostsComponent.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // React Query v4
+
+// Create React Query client
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div style={{ padding: "20px" }}>
-      <PostsComponent />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div style={{ padding: "20px" }}>
+        <h1>React Query Demo</h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
 }
 
